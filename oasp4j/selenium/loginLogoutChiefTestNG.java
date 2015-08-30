82171b0ada9b6976b8e08008f1bdc11fafc1e387
yetisports <dustin@sb-becker.de>
2015-08-30 18:30:10 +0200

    TestNG Initialisierung

--- oasp4j/selenium/loginLogoutChiefTestNG.java
+++ oasp4j/selenium/loginLogoutChiefTestNG.java
@@ -1,3 +1,12 @@
+package com.example.tests;
+
+import com.thoughtworks.selenium.*;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import java.util.regex.Pattern;
+
+public class loginLogoutChiefTestNG extends SeleneseTestNgHelper {
+	@Test public void testLoginLogoutChiefTestNG() throws Exception {
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -36,7 +45,8 @@
 	<td>//div[2]/div/button</td>
 	<td></td>
 </tr>
-
 </tbody></table>
 </body>
 </html>
+	}
+}
