76ab002f5ecfc21295f70f46e85694e3e864ca99
yetisports <dustin@sb-becker.de>
2015-08-30 18:43:56 +0200

    Updated Testcase

--- oasp4j/selenium/loginLogoutChiefTestNG.java
+++ oasp4j/selenium/loginLogoutChiefTestNG.java
@@ -17,10 +17,9 @@ public class loginLogoutChiefTestNG
 
   private WebDriver driver;
 
-  // @SuppressWarnings("deprecation")
   @Test
   public void testLoginLogoutChiefTestNG() throws Exception {
-
+try {
     this.driver = new FirefoxDriver();
     this.driver.get("http://localhost:8081/oasp4j-sample-server/");
     this.driver.findElement(By.name("name=userName")).sendKeys("chief");
@@ -29,6 +28,12 @@ public class loginLogoutChiefTestNG
     this.driver.findElement(By.xpath("//div[2]/div/button")).click();
     this.driver.wait(1000);
     System.out.println("Test war erfolgreich. :D");
+} catch (Exception e) {
+
+}
+finally {
+
     this.driver.quit();
+}
   }
 }
