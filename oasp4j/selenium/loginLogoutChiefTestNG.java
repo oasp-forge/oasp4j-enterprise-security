7a091b20d4dde828e9b48dfc318ac6af34ce89f4
yetisports <dustin@sb-becker.de>
2015-08-31 16:44:00 +0200

    added testng.xml to pom-file

--- oasp4j/selenium/loginLogoutChiefTestNG.java
+++ oasp4j/selenium/loginLogoutChiefTestNG.java
@@ -5,35 +5,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
-import com.thoughtworks.selenium.Selenium;
-
 //import com.thoughtworks.selenium.SeleneseTestNgHelper;
 
 //@SuppressWarnings("javadoc")
 public class loginLogoutChiefTestNG
 // extends SeleneseTestNgHelper
 {
-  private Selenium selenium;
-
   private WebDriver driver;
 
+  // @SuppressWarnings("deprecation")
   @Test
   public void testLoginLogoutChiefTestNG() throws Exception {
-try {
+
     this.driver = new FirefoxDriver();
     this.driver.get("http://localhost:8081/oasp4j-sample-server/");
-    this.driver.findElement(By.name("name=userName")).sendKeys("chief");
-    this.driver.findElement(By.name("name=password")).sendKeys("chief");
+    this.driver.findElement(By.name("userName")).sendKeys("chief");
+    this.driver.findElement(By.name("password")).sendKeys("chief");
     this.driver.findElement(By.cssSelector("button.btn.btn-success")).click();
-    this.driver.findElement(By.xpath("//div[2]/div/button")).click();
-    this.driver.wait(1000);
+    this.driver.findElement(By.cssSelector("button.btn.navbar-btn.btn-sm.btn-info.ng-scope")).click();
     System.out.println("Test war erfolgreich. :D");
-} catch (Exception e) {
-
-}
-finally {
-
     this.driver.quit();
-}
   }
 }
