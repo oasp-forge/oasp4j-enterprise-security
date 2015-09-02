52f03573f6e3e777f7f30cfdcbb2e2179a2c6048
yetisports <dustin@sb-becker.de>
2015-09-02 16:39:45 +0200

    run.sh update

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
+Xvfb :99
+export DISPLAY=:99
 java -cp "/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/bin:/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/lib/*:/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/testng_lib/*" org.testng.TestNG "oasp4j/testng.xml"
