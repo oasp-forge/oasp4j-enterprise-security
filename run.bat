c66208c478ba9dc4f6215fc33b5d35122dc4c235
yetisports <dustin@sb-becker.de>
2015-09-06 17:25:23 +0200

    adapted run.bat to local jenkins installation

--- run.bat
+++ run.bat
@@ -1,3 +1,1 @@
-Xvfb :99
-export DISPLAY=:99
-java -cp "/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/bin:/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/lib/*:/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/testng_lib/*" org.testng.TestNG "oasp4j/testng.xml"
+java -cp "\oasp4j\Selenium\bin:\oasp4j\Selenium\lib\*:\oasp4j\Selenium\testng_lib\*" org.testng.TestNG "oasp4j/testng.xml"
