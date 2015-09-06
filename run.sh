7614013ff4c18d9feea9c510484026203d0c3ec5
yetisports <dustin@sb-becker.de>
2015-09-06 18:43:07 +0200

    added export classpath

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 Xvfb :99
 export DISPLAY=:99
 CLASSPATH=/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/bin:/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/lib/*
+export CLASSPATH
 java -cp "oasp4j/Selenium/bin:oasp4j/Selenium/lib/*" org.testng.TestNG "oasp4j/testng.xml"
