a7f45231382f3becb7f979afc03016808f3982d7
yetisports <dustin@sb-becker.de>
2015-09-08 21:56:59 +0200

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 Xvfb :99
 export DISPLAY=:99
-CLASSPATH=/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/bin:/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/lib/*
-export CLASSPATH
+export CLASSPATH=/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/bin:/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/lib/*
 java -cp "oasp4j/Selenium/bin:oasp4j/Selenium/lib/*" org.testng.TestNG "oasp4j/testng.xml"
