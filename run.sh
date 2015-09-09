9b3951fa1354a5ff053623562601a82ed66faae4
yetisports <dustin@sb-becker.de>
2015-09-09 14:30:10 +0200

    run.sh ignores Xvfb now

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-Xvfb :99
+# Xvfb :99
 export DISPLAY=:99
 export CLASSPATH=/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/bin:/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/lib/*
 java -cp "oasp4j/Selenium/bin:oasp4j/Selenium/lib/*" org.testng.TestNG "oasp4j/testng.xml"
