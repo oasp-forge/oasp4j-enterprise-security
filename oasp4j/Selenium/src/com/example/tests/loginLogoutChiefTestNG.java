c50b3c421398599161256c95246e99b8f86a0078
yetisports <dustin@sb-becker.de>
2015-09-09 16:22:37 +0200

    Added dummy-test and removed wrong class-files

--- oasp4j/Selenium/src/com/example/tests/loginLogoutChiefTestNG.java
+++ oasp4j/Selenium/src/com/example/tests/loginLogoutChiefTestNG.java
@@ -1,5 +1,7 @@
 package com.example.tests;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -16,13 +18,21 @@ public class loginLogoutChiefTestNG
   // @SuppressWarnings("deprecation")
   @Test
   public void testLoginLogoutChiefTestNG() throws Exception {
+
     System.out.println("Test wird gestartet...");
     this.driver = new FirefoxDriver();
-    this.driver.get("http://vm3.rbg.informatik.tu-darmstadt.de:8081/oasp4j-sample-server/");
-    this.driver.findElement(By.name("userName")).sendKeys("chief");
-    this.driver.findElement(By.name("password")).sendKeys("chief");
-    this.driver.findElement(By.cssSelector("button.btn.btn-success")).click();
-    this.driver.findElement(By.cssSelector("button.btn.navbar-btn.btn-sm.btn-info.ng-scope")).click();
+    this.driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    this.driver.get("http://www.1und1.de");
+    this.driver.findElement(By.id("button-hd-nav-webmailer")).click();
+    this.driver.findElement(By.name("emaillogin.Username")).sendKeys("dustin@sb-becker.de");
+    this.driver.findElement(By.name("emaillogin.Password")).sendKeys("schule");
+    this.driver.findElement(By.id("ct-btn-submitbutton-lead")).click();
+    this.driver.findElement(By.className("logout")).click();
+    // this.driver.get("http://vm3.rbg.informatik.tu-darmstadt.de:8081/oasp4j-sample-server/");
+    // this.driver.findElement(By.name("userName")).sendKeys("chief");
+    // this.driver.findElement(By.name("password")).sendKeys("chief");
+    // this.driver.findElement(By.cssSelector("button.btn.btn-success")).click();
+    // this.driver.findElement(By.cssSelector("button.btn.navbar-btn.btn-sm.btn-info.ng-scope")).click();
     System.out.println("Test war erfolgreich. :D");
     this.driver.quit();
   }
