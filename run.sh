3f5a4ad1fb856a46bd6e90b35933aaba44e3e2f9
yetisports <dustin@sb-becker.de>
2015-09-09 16:49:25 +0200

    added ant-file and added the call in run-file

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 # Xvfb :99
 export DISPLAY=:99
 export CLASSPATH=/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/bin:/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/lib/*
+ant -f "oasp4j/build_testng.xml"
 java -cp "oasp4j/Selenium/bin:oasp4j/Selenium/lib/*" org.testng.TestNG "oasp4j/testng.xml"
