a4b70a6188fa4ae477d952ea491170a89cc1112b
yetisports <dustin@sb-becker.de>
2015-09-10 07:19:01 +0200

    volle rechte für testclass

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 export DISPLAY=:99
 export CLASSPATH=/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/bin:/var/lib/jenkins/workspace/SampleApplication/oasp4j/Selenium/lib/*
 ant -f "oasp4j/build_testng.xml"
+chmod +x "oasp4j/Selenium/bin/com/example/tests/loginLogoutChiefTestNG.class"
 java -cp "oasp4j/Selenium/bin:oasp4j/Selenium/lib/*" org.testng.TestNG "oasp4j/testng.xml"
